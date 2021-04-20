6104a157e70aec1c375f9b18e80633bfb30c6c30
Bastien <bzg@gnu.org>
2021-04-20 09:32:54 +0200

    Bump version number

--- project.clj
+++ project.clj
@@ -3,20 +3,20 @@
 ;; License-Filename: LICENSES/EPL-2.0.txt
 
 (defproject
-  subscribe "0.7.3"
+  subscribe "0.7.4"
   :url "https://github.com/bzg/subscribe"
   :description "Web application to subscribe to mailing lists."
   :license {:name "Eclipse Public License v2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.async "1.3.610"
                   :exclusions [org.clojure/tools.reader]]
                  [compojure "1.6.2"]
                  [hiccup "1.0.5"]
-                 [ring/ring-core "1.9.1"]
-                 [ring/ring-jetty-adapter "1.9.1"]
-                 [ring/ring-devel "1.9.1"]
+                 [ring/ring-core "1.9.2"]
+                 [ring/ring-jetty-adapter "1.9.2"]
+                 [ring/ring-devel "1.9.2"]
                  [ring/ring-defaults "0.3.2"]
                  [cheshire "5.10.0"]
                  [org.martinklepsch/clj-http-lite "0.4.3"]
