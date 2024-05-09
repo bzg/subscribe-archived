92e4d50595fae0ee439253a34b45733c3c347970
Bastien Guerry <bzg@gnu.org>
2024-05-09 19:21:09 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -9,20 +9,20 @@
   :license {:name "Eclipse Public License v2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
                  [org.clojure/core.async "1.6.681"
                   :exclusions [org.clojure/tools.reader]]
-                 [compojure "1.7.0"]
+                 [compojure "1.7.1"]
                  [hiccup "1.0.5"]
-                 [ring/ring-core "1.11.0"]
-                 [ring/ring-jetty-adapter "1.11.0"]
-                 [ring/ring-devel "1.11.0"]
-                 [ring/ring-defaults "0.4.0"]
-                 [cheshire "5.12.0"]
+                 [ring/ring-core "1.12.1"]
+                 [ring/ring-jetty-adapter "1.12.1"]
+                 [ring/ring-devel "1.12.1"]
+                 [ring/ring-defaults "0.5.0"]
+                 [cheshire "5.13.0"]
                  [org.martinklepsch/clj-http-lite "0.4.3"]
                  [com.taoensso/tempura "1.5.3"]
                  [com.draines/postal "2.0.5"]
-                 [com.taoensso/timbre "6.3.1"]
+                 [com.taoensso/timbre "6.5.0"]
                  [commons-validator "1.8.0"]]
   :main core
   :jvm-opts ["-Xmx500m"]
