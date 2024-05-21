3958cf57212a91b459f79902fbe42761923f7fa7
Bastien Guerry <bzg@gnu.org>
2024-05-21 13:54:55 +0200

    Update copyright years

--- src/core.clj
+++ src/core.clj
@@ -1,4 +1,4 @@
-;; Copyright (c) 2019-2023 Bastien Guerry <bzg@bzg.fr>
+;; Copyright (c) 2019-2024 Bastien Guerry <bzg@bzg.fr>
 ;; SPDX-License-Identifier: EPL-2.0
 ;; License-Filename: LICENSES/EPL-2.0.txt
 
