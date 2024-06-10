11a44e89837287610c19a16d46ce1ecf5a7ce414
Bastien Guerry <bzg@gnu.org>
2024-06-10 16:05:53 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -9,21 +9,26 @@
   :license {:name "Eclipse Public License v2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.11.3"]
-                 [org.clojure/core.async "1.6.681"
-                  :exclusions [org.clojure/tools.reader]]
-                 [compojure "1.7.1"]
-                 [hiccup "1.0.5"]
-                 [ring/ring-core "1.12.1"]
-                 [ring/ring-jetty-adapter "1.12.1"]
-                 [ring/ring-devel "1.12.1"]
-                 [ring/ring-defaults "0.5.0"]
-                 [cheshire "5.13.0"]
-                 [org.martinklepsch/clj-http-lite "0.4.3"]
-                 [com.taoensso/tempura "1.5.3"]
+  :dependencies [
+                 ;; [integrant/integrant "0.10.0"]
+                 [cheshire/cheshire "5.13.0"]
                  [com.draines/postal "2.0.5"]
+                 [com.taoensso/tempura "1.5.4"]
                  [com.taoensso/timbre "6.5.0"]
-                 [commons-validator "1.8.0"]]
+                 [commons-validator/commons-validator "1.9.0"]
+                 [hiccup/hiccup "1.0.5"]
+                 [http-kit/http-kit "2.8.0"]
+                 [markdown-clj/markdown-clj "1.12.1"]
+                 [metosin/reitit "0.7.0"]
+                 [metosin/reitit-middleware "0.7.0"]
+                 [metosin/reitit-ring  "0.7.0"]
+                 [org.clojure/clojure "1.11.3"]
+                 [org.clojure/core.async "1.6.681"]
+                 [org.martinklepsch/clj-http-lite "0.4.3"]
+                 [ring-cors/ring-cors "0.1.13"]
+                 [ring/ring-devel "1.12.1"]
+                 [selmer/selmer "1.12.61"]
+                 ]
   :main core
   :jvm-opts ["-Xmx500m"]
   :profiles {:uberjar {:omit-source    true
